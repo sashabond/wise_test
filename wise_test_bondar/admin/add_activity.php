5dba49d7598ebcf873273637902f0920dd1546f0
sashabond <78707006+sashabond@users.noreply.github.com>
2021-03-19 15:17:11 +0200

    Add files via upload

--- wise_test_bondar/admin/add_activity.php
+++ wise_test_bondar/admin/add_activity.php
@@ -6,8 +6,14 @@ $end_t = $_POST['end_time'];
 $distance = $_POST['distance'];
 $type = $_POST['type'];
 $date = date('F j');
+
 if($start_t == 0 || $end_t == 0 || $distance == 0 || $type == NULL ){    //перевірка на ввід данних
-    echo"Введіть дані";
+    echo '<script>
+             alert( "Введіть дані за зразком нижче: \n " +
+             "Для Start time:  hh:mm:ss (exm: 15:43) \n " +
+              "Для Finish time:  hh:mm:ss (exm: 15:43) \n" +
+               " Для Distance:  num of meters (exm: 8556)" );
+          </script>';
 
 }
 else{
