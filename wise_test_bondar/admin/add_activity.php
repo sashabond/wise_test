df941e998c06816cb9814e8f2212f08a4dd1acfa
sashabond <78707006+sashabond@users.noreply.github.com>
2021-03-19 14:26:26 +0200

    Add files via upload

--- wise_test_bondar/admin/add_activity.php
+++ wise_test_bondar/admin/add_activity.php
@@ -6,5 +6,11 @@ $end_t = $_POST['end_time'];
 $distance = $_POST['distance'];
 $type = $_POST['type'];
 $date = date('F j');
+if($start_t == 0 || $end_t == 0 || $distance == 0 || $type == NULL ){    //перевірка на ввід данних
+    echo"Введіть дані";
+
+}
+else{
 mysqli_query($link, "INSERT INTO `activ_info`(`id`, `data`, `distance`, `start_time`, `end_time`, `type`) VALUES (NULL, '$date', '$distance', '$start_t', '$end_t', '$type')");
 header('Location: ../index.php');
+}
